374d1f3e7d42b98bbc78b557a5e39619b32542fa
RogueKatoz <adam.katic1@gmail.com>
2026-04-02 12:24:14 +1030

    Add required card functions from assignment description: virtual str(), play() and willAddToBank();

--- DeadMansDraw/include/Card.h
+++ DeadMansDraw/include/Card.h
@@ -1,7 +1,24 @@
 #pragma once
+
+#include "Game.h"
+#include "Player.h"
+
 #include <vector>
 
 class Card;
 typdef std::vector<Card*> CardCollection;
 
 enum CardType {Cannon, Chest, Key, Anchor, Sword, Hook, Oracle, Map, Mermaid, Kraken};
+
+class Card {
+protected:
+	CardType _type;
+	int _value;
+
+public:
+	const CardType& type() const;
+	int value();
+	virtual std::string str() const = 0;
+	virtual void play(Game& game, Player& player) = 0;
+	virtual void willAddToBank(Game& game, Player& player) = 0;
+};

4ce2d035223d773515410b427cf1b3924ff01f99
RogueKatoz <adam.katic1@gmail.com>
2026-04-07 13:54:54 +0930

    Add ability for kraken cards.

--- DeadMansDraw/src/cards/KrakenCard.cpp
+++ DeadMansDraw/src/cards/KrakenCard.cpp
@@ -1,5 +1,6 @@
 #include "KrakenCard.h"
 #include <string>
+#include <iostream>
 
 KrakenCard::KrakenCard(int value)
 {
@@ -13,4 +14,14 @@ std::string KrakenCard::str() const
 
 void KrakenCard::play(Game& game, Player& player)
 {
+	std::cout << "\tDraw 3 cards from the deck and play each:" << std:endl;
+	
+	for (int i = 0; i < 3; i++)
+	{
+		Card* drawnCard = game.drawCard();
+		if (drawnCard != nullptr && player.isBust() == 0)
+		{
+			player->playCard(drawnCard, game);
+		}
+	}
 }

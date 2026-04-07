b53427aa7cdc7fcdfda49241b0b2034d08bf83b5
RogueKatoz <adam.katic1@gmail.com>
2026-04-07 10:14:13 +0930

    Finish logic for playing Cannon cards.

--- DeadMansDraw/src/cards/CannonCard.cpp
+++ DeadMansDraw/src/cards/CannonCard.cpp
@@ -1,5 +1,6 @@
 #include "CannonCard.h"
 #include <string>
+#include <iostream>
 
 CannonCard::CannonCard(int value)
 {
@@ -14,5 +15,13 @@ std::string CannonCard::str() const
 void CannonCard::play(Game& game, Player& player)
 // If other player's bank has cards, steal the highest value card from suit of player's choice and discard it.
 {	
-	
+	if (game->otherPlayer()->bankHasCards() == 0)
+	{
+		std::cout << "No cards in other player's bank. Play continues." << std::endl;
+		return;
+	}
+
+	std::cout << "\tShoot the top card of any suit from the other player's Bank into the Discard Pile:" << std::endl;
+	Card* cardToDiscard = game->otherPlayer()->stealBankCard();
+	game->discardCard(*cardToDiscard);
 }

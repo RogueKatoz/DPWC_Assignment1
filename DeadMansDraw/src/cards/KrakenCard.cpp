edcf67306181197a66e6f8eafd52a0f14773b699
RogueKatoz <adam.katic1@gmail.com>
2026-04-07 17:57:15 +0930

    Fix missing semicolon in Kraken card

--- DeadMansDraw/src/cards/KrakenCard.cpp
+++ DeadMansDraw/src/cards/KrakenCard.cpp
@@ -14,7 +14,7 @@ std::string KrakenCard::str() const
 
 void KrakenCard::play(Game& game, Player& player)
 {
-	std::cout << "\tDraw 3 cards from the deck and play each:" << std:endl;
+	std::cout << "\tDraw 3 cards from the deck and play each:" << std::endl;
 	
 	for (int i = 0; i < 3; i++)
 	{
